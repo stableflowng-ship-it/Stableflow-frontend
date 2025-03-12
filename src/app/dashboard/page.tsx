a0e3b11921705beb6159c1f586ea6cca8bb1f143
Captain-awesome19 <ogunsolatoluwalase@gmail.com>
2025-03-12 01:48:15 +0100

    edit

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,7 +5,7 @@ import logo from '../../assests/image1.png'
 import address from '../../assests/image4.svg'
 import { VolumeWidget } from '@/components'
 import {TransactionHistoryContainer} from '@/components'
-
+import edit from '../../assests/magicpen.svg'
 const page = () => {
 
   function Copy() {
@@ -35,10 +35,21 @@ const page = () => {
    
     <div className="grid pt-[2%]  overflow-hidden px-[rem] items-start justify-center gap-[3rem] lg:gap-[2rem] h-full   bg-cover bg-center" style={{ backgroundImage: "url('/image3.svg')", backgroundPosition : "center" }}>
    <div className=' grid w-screen lg:w-full '>
-   <button className='grid decoration-0 cursor-pointer ml-[1rem] w-[90%] border-[1px] p-[0.5rem] bg-[#EFEFEF] rounded-3xl border-transparent'>
+   <div className='flex items-center justify-between ml-[1rem] w-[90%] border-[1px] p-[0.5rem] bg-[#EFEFEF] rounded-3xl border-transparent'>
+   <button className='grid decoration-0 cursor-pointer '>
    <h3 className=' text-[18px] lg:text-[25px] flex  text-[#828282] font-[500]'>Hola👋,</h3>
    <h3 className='text-[18px] lg:text-[25px] text-[#121212] underline underline-offset-1 flex items-start font-[500]'>Enter your business name</h3>
    </button>
+   <div className='cursor-pointer flex p-[0.5rem] bg-white border-[1px] border-white rounded-3xl  shadow-md shadow-gray-200 items-center justify-center gap-[0.4rem] lg:gap-[0.5rem]'>
+   <Image
+   src={edit}
+   alt='edit'
+   height={20}
+   width={20}
+   />
+<h3>Edit name</h3>
+   </div>
+   </div>
    <VolumeWidget />
    </div>
    
